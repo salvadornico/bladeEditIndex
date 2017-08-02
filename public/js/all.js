20aa27ba8c4ab4e9119e68196c4b257954d092d5
Nico Salvador <salvador.nico@gmail.com>
2017-08-02 10:57:38 +0800

    Applied consistent nav syling

--- public/js/all.js
+++ public/js/all.js
@@ -1,17 +1,5 @@
 $(document).ready(function() {
 	$('ul.tabs').tabs()
-	tabs = $('li.tab')
-	tabLinks = $('li.tab > a')
-
-	for (var i = 0; i < tabLinks.length; i++) {
-		if (tabLinks[i].textContent.trim() == activePage) {
-			tabLinks[i].className = "active"
-			tabs[i].className += " active"
-		} else {
-			tabLinks[i].className = ""
-		}
-	}
-
 	$('.modal').modal()
 })
 

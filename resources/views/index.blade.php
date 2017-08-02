43a08c826635b18f7586896752276b0e41b4a615
Nico Salvador <salvador.nico@gmail.com>
2017-08-02 12:33:57 +0800

    Moved to grid layout for home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,15 @@
 	<div class="container">
 		
 		<div class="row section" id="topRow">
+
+			<div id="randomVid">
+
+				<h4 class="hide-on-small-only">Random video</h4>
+				@include("layouts.player_partial")
+
+			</div>
 			
-			<div class="col s12 m6 l7" id="intro">
+			<div id="intro">
 				<h1>Yo</h1>
 				<p>
 					This site is for all of us who could never quite find that one edit.
@@ -14,7 +21,9 @@
 				<p>
 					Everything on here is entirely dependent on you, the blading community. So please add all your favorite edits and tag them as thoroughly as you can to help others find them in the future. The more you contribute, the more useful this site becomes for everyone!
 				</p>
+			</div>
 
+			<div id="getStarted">
 				<h5 class="center-align">Get started:</h5>
 				<a href="{{ url("/addVideo") }}" class="btn waves-effect waves-light amber darken-1">
 					Add a video
@@ -23,13 +32,6 @@
 				<p>
 					<span class="amber-text text-accent-3">WARNING:</span> This site is currently still under development. Please send your feedback to <a href="mailto:whatsthatedit@gmail.com">whatsthatedit@gmail.com</a> and we'll do our best to squash out bugs as we find them.
 				</p>
-			</div>
-
-			<div class="col m5 offset-m1 l4 offset-l1 hide-on-small-only" id="randomVid">
-
-				<h4>Random video</h4>
-				@include("layouts.player_partial")
-
 			</div>
 
 		</div> {{-- /top row --}}
